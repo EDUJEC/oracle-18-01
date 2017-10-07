61c0a7a4a2b2e13a0e4fc006209095c387cb1274
EDUJEC <31869863+EDUJEC@users.noreply.github.com>
2017-10-07 10:47:49 -0500

    Add files via upload

--- cursobd2.sql
+++ cursobd2.sql
@@ -1,7 +1,28 @@
 
 -- Clase de hoy sabado 7 de octubre
-
 create table almacen (numero_almacen integer, 
                       ubicacion_alamacen varchar2(80),
                       constraint pk_na primary key(numero_almacen)
                       );
+
+-- VAMOS AGENERARUN PROCEDIMIENTO ALMACENADO PARA GUARDAR UNA ENTIDAD O REGISTRO DE TIPO ALMACEN  
+create or replace procedure guardar_almacen(mi_id IN integer,
+                                            mi_ubicacion IN varchar2)
+                                            as
+                                            begin
+                          --AQUI BINE LA LOGICA DEL PROCEDIMIENTO
+                          
+                          insert into almacen values(mi_id,mi_ubicacion);
+                          end;
+                          /
+
+--VAMOS A PROBAR QUE FUNCIONA NUESTRO PROCEDIMIENTO
+begin
+guardar_almacen(1,'PLYMOUTH');
+end;
+/
+
+SELECT * FROM almacen;
+
+
+                          
